def21e0fadb0392c4ca757fec6c720195521d23a
易伟平 <doomsdayloveft@163.com>
2016-05-01 19:12:05 +0800

    issue:none msg:none

---
+++ src/test/java/com/scaltoy/MainTest.java
@@ -0,0 +1,11 @@
+package com.scaltoy;
+
+/**
+ * Created by acer on 2016/5/1.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
